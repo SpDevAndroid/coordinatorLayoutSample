bb8908898836bf9a1e7061b37e626b285ad9ed99
Shubham.Pundir <shubham.pundir@timesinternet.in>
2023-01-31 20:39:32 +0530

    changes for sample for toolbar collapse on scroll smooth

--- app/src/main/java/saulmm/coordinatorexamples/SimpleCoordinatorActivity.java
+++ app/src/main/java/saulmm/coordinatorexamples/SimpleCoordinatorActivity.java
@@ -62,52 +62,56 @@ public class SimpleCoordinatorActivity extends AppCompatActivity implements AppB
 
     @Override
     public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
-//        double offsetAlpha = -(appbarLayout.getY() / appbarLayout.getTotalScrollRange());
-//        /**Here 0.7 signifies the 7 percent of the screen from where animation will start on offset scroll*/
-//        if (offsetAlpha > 0.1) {
-//            showToolbarWithAnim();
-//            /** Hide Logo collapsable view when showing toolbar **/
+        double offsetAlpha = -(appbarLayout.getY() / appbarLayout.getTotalScrollRange());
+        /**Here 0.7 signifies the 7 percent of the screen from where animation will start on offset scroll*/
+        if (offsetAlpha > 0.2) {
 //            cardCoupon.setVisibility(View.INVISIBLE);
-//            llTabs.setVisibility(View.INVISIBLE);
-//            tvSearchBar.setVisibility(View.INVISIBLE);
-//        } else {
-//            hideToolbarWithAnim();
-//            /** Show Logo collapsable view when hiding toolbar **/
-//            cardCoupon.setVisibility(View.VISIBLE);
-//            llTabs.setVisibility(View.VISIBLE);
-//            tvSearchBar.setVisibility(View.VISIBLE);
-//        }
+            llTabs.setVisibility(View.INVISIBLE);
+            tvSearchBar.setVisibility(View.INVISIBLE);
+            showToolbarWithAnim();
+            /** Hide Logo collapsable view when showing toolbar **/
+
+        } else {
+            hideToolbarWithAnim();
+            /** Show Logo collapsable view when hiding toolbar **/
+
+        }
     }
 
     private void hideToolbarWithAnim() {
         if (toolbarCollapsed.getVisibility() == View.VISIBLE) {
-            Animation animationDown = AnimationUtils.loadAnimation(this, R.anim.view_close_from_bottom_up);
-            toolbarCollapsed.startAnimation(animationDown);
-            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-                @Override
-                public void run() {
+//            Animation animationDown = AnimationUtils.loadAnimation(this, R.anim.view_close_from_bottom_up);
+//            toolbarCollapsed.startAnimation(animationDown);
+//            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
                     toolbarCollapsed.setVisibility(View.INVISIBLE);
                     toolbarCollapsed.setClickable(false);
                     toolbarCollapsed.setFocusable(false);
                     toolbar.setVisibility(View.VISIBLE);
-                }
-            }, 300);
+
+//                    cardCoupon.setVisibility(View.VISIBLE);
+                    llTabs.setVisibility(View.VISIBLE);
+                    tvSearchBar.setVisibility(View.VISIBLE);
+
+//                }
+//            }, 50);
         }
     }
 
     private void showToolbarWithAnim() {
         if (toolbarCollapsed.getVisibility() == View.INVISIBLE) {
-            Animation animationDown = AnimationUtils.loadAnimation(this, R.anim.slide_from_up);
-            toolbarCollapsed.startAnimation(animationDown);
-            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-                @Override
-                public void run() {
+//            Animation animationDown = AnimationUtils.loadAnimation(this, R.anim.slide_from_up);
+//            toolbarCollapsed.startAnimation(animationDown);
+//            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
                     toolbarCollapsed.setVisibility(View.VISIBLE);
                     toolbar.setVisibility(View.INVISIBLE);
                     toolbarCollapsed.setClickable(true);
                     toolbarCollapsed.setFocusable(true);
-                }
-            }, 300);
+//                }
+//            }, 50);
         }
 
     }
